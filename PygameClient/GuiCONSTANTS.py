a453fc1c69a7a27c86f6f4ed37cabdea220246ed
Frostlock <pieter.boogaerts@gmail.com>
2019-05-17 20:51:17 +0200

    PEP 8 and small fixes

--- PygameClient/GuiCONSTANTS.py
+++ PygameClient/GuiCONSTANTS.py
@@ -1,35 +1,35 @@
-'''
-Created on Mar 21, 2014
+"""
+Created on Mar 21,  2014
 
 @author: pi
 
 This module contains all the constants that are used by the application.
 Note that all constants are CAPITAL letters only for clarity.
-'''
+"""
 
-APPLICATION_NAME = "W@rrens"
+APPLICATION_NAME = "W@rrens II"
 
 # RGB Colors
-COLOR_UNEXPLORED = (0,0,0)
-COLOR_BLOCKED = (27,27,27)
-COLOR_NOTBLOCKED = (47,47,47)
-# COLOR_WALL = (100,76,20)
-# COLOR_GROUND = (139,105,20)
+COLOR_UNEXPLORED = (0, 0, 0)
+COLOR_BLOCKED = (27, 27, 27)
+COLOR_NOTBLOCKED = (47, 47, 47)
+# COLOR_WALL = (100, 76, 20)
+# COLOR_GROUND = (139, 105, 20)
 
-COLOR_PANEL = (0,0,0)
-COLOR_PANEL_FONT = (191,191,191)
+COLOR_PANEL = (0, 0, 0)
+COLOR_PANEL_FONT = (191,  191,  191)
 
-COLOR_SELECT = (255,0,0)
-COLOR_POPUP = (243,0,0)
+COLOR_SELECT = (255,  0,  0)
+COLOR_POPUP = (243,  0,  0)
 
-COLOR_BAR_HEALTH = (150,0,0)
-COLOR_BAR_HEALTH_BG = (25,0,0)
-COLOR_BAR_XP = (0,150,0)
-COLOR_BAR_XP_BG = (0,25,0)  
+COLOR_BAR_HEALTH = (150,  0,  0)
+COLOR_BAR_HEALTH_BG = (25,  0,  0)
+COLOR_BAR_XP = (0, 150, 0)
+COLOR_BAR_XP_BG = (0, 25, 0)  
 
-COLOR_FONT = (191,191,191)
+COLOR_FONT = (191, 191, 191)
 
-# RGB + Alpha transparancy
+# RGB + Alpha transparency
 COLOR_MENU_BG = (0, 0, 0, 125)
 
 
