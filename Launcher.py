83c3a49022be8705111dec9a58fff396187c6f1f
Frostlock <pieter.boogaerts@gmail.com>
2019-06-12 20:39:23 +0200

    Launcher docs

--- Launcher.py
+++ Launcher.py
@@ -1,8 +1,13 @@
 from WarrensClient.GuiApplication import GuiApplication
 
-"""
-Launcher script
-"""
-if __name__ == '__main__':
+
+def Launch():
+    """
+    Launches the GuiApplication with splashscreen.
+    """
     _application = GuiApplication()
     _application.show_splash_screen()
+
+
+if __name__ == '__main__':
+    Launch()
