f9d5264ce49061dc4bd74fc12528874d2706cb24
Frostlock <pieter.boogaerts@gmail.com>
2019-06-17 21:32:03 +0200

    pyinstaller packaging WIP

--- Launcher.py
+++ Launcher.py
@@ -1,4 +1,6 @@
 from WarrensClient.Application import Application
+import os
+import sys
 
 
 def Launch():
@@ -10,4 +12,7 @@ def Launch():
 
 
 if __name__ == '__main__':
+    # The following ensures that the working directory is set correctly when executing through a pyinstaller generated package.
+    if getattr(sys, 'frozen', False):
+        os.chdir(sys._MEIPASS)
     Launch()
--- pyinstaller/pyinstaller-package.sh
+++ pyinstaller/pyinstaller-package.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-pyinstaller -y --distpath ../dist onedir.spec
+# pyinstaller -y --distpath ../dist onedir.spec
 
 # Not working properly, needs a different spec file
 #pyinstaller -y --onefile --distpath ../dist onefile.spec
 
 # For some reason the data files are not added properly
-#pyinstaller --noconfirm \
-#    --add-data="../WarrensClient/assets/*:WarrensClient/assets" \
-#    --add-data="../WarrensClient/music/*:WarrensClient/music" \
-#    --add-data="../WarrensClient/sfx/*:WarrensClient/sfx" \
-#    --add-data="../WarrensGame/*.csv:WarrensGame" \
-#    ../Launcher.py
+# pyinstaller --clean --noconfirm --onefile \
+# --runtime-tmpdir "." \
+pyinstaller --clean --noconfirm \
+    --distpath="../dist" \
+    --add-data="../WarrensClient/assets/*:WarrensClient/assets" \
+    --add-data="../WarrensClient/music/*:WarrensClient/music" \
+    --add-data="../WarrensClient/sfx/*:WarrensClient/sfx" \
+    --add-data="../WarrensGame/*.csv:WarrensGame" \
+    --onefile \
+    ../Launcher.py
